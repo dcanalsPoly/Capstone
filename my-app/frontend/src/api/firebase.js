0b0880dab472b550b87a516c31be5a75b1cbf33b
Diego Canals <canals_80877@students.pupr.edu>
2021-03-02 21:36:16 -0400

    Added frontend and backend dirs

---
+++ my-app/frontend/src/api/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAyKax6VJyHsIzrfM2QAgwhXkcYVoqGmaY",
+    authDomain: "capstone-e4492.firebaseapp.com",
+    projectId: "capstone-e4492",
+    storageBucket: "capstone-e4492.appspot.com",
+    messagingSenderId: "481311685755",
+    appId: "1:481311685755:web:9df03db44aff7feb2bb6de"
+  };
