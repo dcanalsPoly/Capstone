897e9bbf8f60712945fc827d739bbeec85c8d4de
Elias <marrero_97850@students.pupr.edu>
2021-04-11 02:44:16 -0400

    fixed Colorblind button's state

--- my-app/frontend/src/components/ColorblindButton.js
+++ my-app/frontend/src/components/ColorblindButton.js
@@ -1,6 +1,7 @@
 import './ColorblindButton.css';
 import React, { useState } from 'react';
 import './styling/customLabel.css';
+import { IoColorPaletteSharp } from 'react-icons/io5';
 
 function ColorblindButton(props) {
     
@@ -9,41 +10,66 @@ function ColorblindButton(props) {
     const [colorblindType, setColorblindType] = useState("Default");
     console.log(colorblindType);
 
-    return <div className="colorblind">
-        <span className="label customH2Dark">Colorblindness: </span>
-        <select className="dropButton customH2Dark" onChange={
-            (e) => {
-                    setColorblindType(e.target.value);
-                    if (colorblindType === "Default")
-                    {
-                        props.setLightColor('#007965');
-                        props.setNormalColor('#018383');
-                        props.setDarkColor('#2d6187');
-                        //
-                        props.setLineColor('#75daad');
-                    }
-                    else if (colorblindType === "Deuteranopia")
-                    {
-                        props.setLightColor('#f9f871');
-                        props.setNormalColor('#fddb3a');
-                        props.setDarkColor('#fff600');
-                        //
-                        props.setLineColor('#28527a');
-                    }
-                    else if (colorblindType === "Tritanopia")
-                    {
-                        props.setLightColor('#e84545');
-                        props.setNormalColor('#e40017');
-                        props.setDarkColor('#91091e');
-                        //
-                        props.setLineColor('#28527a');
-                    }
-                }
-            }>
-            <option className="dropdownItems" value="Default">Default</option>
-            <option className="dropdownItems" value="Deuteranopia">Deuteranopia</option>
-            <option className="dropdownItems" value="Tritanopia">Tritanopia</option>
-        </select>
-    </div>
+    const [showDropdown, setShowDropdown] = useState("false");
+
+    //do fucntion that when you tap the dropdown item, sets value of colorblindType, 
+    //and triggers the showDropdown to the contrary
+    function onClickDropItem (type) {
+        showDropdown === "false" ? setShowDropdown("true") : setShowDropdown("false");
+        if (type === "Default")
+        {
+            props.setLightColor('#007965');
+            props.setNormalColor('#018383');
+            props.setDarkColor('#2d6187');
+            //
+            props.setLineColor('#75daad');
+        }
+        else if (type === "Deuteranopia")
+        {
+            props.setLightColor('#f9f871');
+            props.setNormalColor('#fddb3a');
+            props.setDarkColor('#fff600');
+            //
+            props.setLineColor('#28527a');
+        }
+        else if (type === "Tritanopia")
+        {
+            props.setLightColor('#e84545');
+            props.setNormalColor('#e40017');
+            props.setDarkColor('#91091e');
+            //
+            props.setLineColor('#28527a');
+        }
+
+    }
+
+    if (showDropdown === "false")
+        return <div className="dropdownButton">
+            <span onClick={() => showDropdown === "false" ? setShowDropdown("true") : setShowDropdown("false")}>
+                <IoColorPaletteSharp className="dropIcon"></IoColorPaletteSharp>
+            </span>
+        </div>
+    else {
+        return <div className="dropdownColumn">
+            <div className="dropdownButton">
+                <span onClick={() => showDropdown === "false" ? setShowDropdown("true") : setShowDropdown("false")}>
+                    <IoColorPaletteSharp className="dropIcon"></IoColorPaletteSharp>
+                </span>
+            </div>
+            
+            <div className="dropdownItemsContainer">
+                <span className="dropdownItems customH3" onClick={() => onClickDropItem("Default")}>
+                Default
+                </span>
+                <span className="dropdownItems customH3" onClick={() => onClickDropItem("Deuteranopia")}>
+                Deuteranopia
+                </span>
+                <span className="dropdownItems customH3" onClick={() => onClickDropItem("Tritanopia")}>
+                Tritanopia
+                </span>
+            </div>
+            
+        </div>
+    }
 }
 export default ColorblindButton;
--- my-app/frontend/src/components/Content.js
+++ my-app/frontend/src/components/Content.js
@@ -6,11 +6,6 @@ import React, { useState } from 'react';
 
 function Content() {
 
-<<<<<<< HEAD
-    //green&blue default =          #007965, #018383, #2d6187,, #75daad
-    //blue&yellow deuteronopia =    #f9f871, #fddb3a, #fff600,, #28527a
-    //red&blue tritanopia =         #e84545, #e40017, #91091e,, #28527a
-=======
     const [graphTypeState, setGraphTypeState] = useState("casesByDay");
 
     var graphKind;
@@ -64,20 +59,18 @@ function Content() {
     //green&blue default =  #007965, #018383, #2d6187,, #75daad
     //blue&yellow deuteronopia = #f9f871, #fddb3a, #fff600,, #28527a
     //red&blue tritanopia = #e84545, #e40017, #91091e,, #28527a
->>>>>>> aa6a7f63f5630b5f47c4603f165bcc14b1161ef4
 
     const [lightColor, setLightColor] = useState('#007965'); 
     const [normalColor, setNormalColor] = useState('#018383'); 
     const [darkColor, setDarkColor] = useState('#2d6187'); 
     const [lineColor, setLineColor] = useState('#75daad');
 
-    
-
-
-
     return <div className="content">
-        <ColorblindButton setLightColor={setLightColor} setNormalColor={setNormalColor} setDarkColor={setDarkColor} setLineColor={setLineColor}/>
-        <GraphSelector graphType={graphTypeState} setGraphType={setGraphTypeState}/>
+        <div className="contentRow">
+            <span></span>
+            <GraphSelector graphType={graphTypeState} setGraphType={setGraphTypeState}/>
+            <ColorblindButton setLightColor={setLightColor} setNormalColor={setNormalColor} setDarkColor={setDarkColor} setLineColor={setLineColor}/>
+        </div>
         <Graph graph={graphKind} xAxis={graphXAxis} yAxis={graphYAxis} Label={graphLabel} lightColor={lightColor} normalColor={normalColor} darkColor={darkColor} lineColor={lineColor}/>
     </div>
 }
